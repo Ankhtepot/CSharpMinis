b9da6e73ccb655eb4b70a9507a15090532741670
Zavodny Petr <petr.zavodny@sensearena.com>
2025-12-18 12:19:13 +0100

    ArgumentsOptions isolating args processing from main

--- LogsParser/ArgumentsOption.cs
+++ LogsParser/ArgumentsOption.cs
@@ -2,5 +2,47 @@ namespace LogsParser;
 
 public class ArgumentsOption
 {
-    public required ILogParseMethod ParseMethod;
+    public ILogParseMethod ParseMethod;
+
+    private ArgumentsOption()
+    {
+        ParseMethod = new LogParseIds();
+    }
+
+    public static ArgumentsOption Create(string[] args)
+    {
+        return ProcessArguments(args);
+    }
+    
+    private static ArgumentsOption ProcessArguments(string[]? args = null)
+    {
+        ArgumentsOption options = new();
+        
+        if (args == null || args.Length == 0)
+        {
+            options.ParseMethod = new LogParseIds();
+        }
+
+        foreach (string argument in args)
+        {
+            switch (argument)
+            {
+                case "--help":
+                case "-h":
+                    Console.WriteLine("Usage: LogsParser [--numeric | --alphanumeric]");
+                    Console.WriteLine("--numeric       Use numeric IDs only.");
+                    Console.WriteLine("--alphanumeric  Use alphanumeric IDs (default).");
+                    Environment.Exit(0);
+                    break;
+                case "--numeric":
+                    options.ParseMethod = new LogParseNumericIds();
+                    break;
+                case "--alphanumeric":
+                    options.ParseMethod = new LogParseIds();
+                    break;
+            }
+        }
+        
+        return options;
+    }
 }
--- LogsParser/Program.cs
+++ LogsParser/Program.cs
@@ -7,49 +7,9 @@ internal static class Program
         using HttpClient client = new();
         string logString = await client.GetStringAsync("https://coderbyte.com/api/challenges/logs/web-logs-raw");
 
-        ArgumentsOption options = ProcessArguments(args);
+        ArgumentsOption options = ArgumentsOption.Create(args);
 
         options.ParseMethod.SetData(logString, out Dictionary<string, int> counts);
         options.ParseMethod.WriteResult(counts);
-    }
-
-    private static ArgumentsOption ProcessArguments(string[]? args = null)
-    {
-        if (args == null || args.Length == 0)
-        {
-            return new ArgumentsOption
-            {
-                ParseMethod = new LogParseIds()
-            };
-        }
-
-        foreach (string argument in args)
-        {
-            switch (argument)
-            {
-                case "--help":
-                case "-h":
-                    Console.WriteLine("Usage: LogsParser [--numeric | --alphanumeric]");
-                    Console.WriteLine("--numeric       Use numeric IDs only.");
-                    Console.WriteLine("--alphanumeric  Use alphanumeric IDs (default).");
-                    Environment.Exit(0);
-                    break;
-                case "--numeric":
-                    return new ArgumentsOption()
-                    {
-                        ParseMethod = new LogParseNumericIds()
-                    };
-                case "--alphanumeric":
-                    return new ArgumentsOption()
-                    {
-                        ParseMethod = new LogParseIds()
-                    };
-            }
-        }
-        
-        return new ArgumentsOption
-        {
-            ParseMethod = new LogParseIds()
-        };
     }
 }
