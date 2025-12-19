605030a5a9799940a8f5f925249425494f441413
Zavodny Petr <petr.zavodny@sensearena.com>
2025-12-19 13:20:31 +0100

    SempaphorSlim mini

---
+++ SemaphorSlim/Program.cs
@@ -0,0 +1,24 @@
+﻿public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        SemaphoreSlim semaphore = new(3);
+        IEnumerable<int> range = Enumerable.Range(1, 12);
+        
+        await Task.WhenAll(range.Select(number => ProcessAsync(number, semaphore)));
+    }
+    
+    private static  async Task ProcessAsync(int id, SemaphoreSlim semaphore)
+    {
+        await semaphore.WaitAsync();
+        try
+        {
+            Console.WriteLine($"Task {id} started as {DateTime.Now:T}");
+            await Task.Delay(1000); // Simulate work
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+}
